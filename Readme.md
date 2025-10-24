87f4750238bd41415d2d59ddf3de1312e5739103
Ariel Maniago <admaniago01@gmail.com>
2025-10-24 18:03:13 +0800

    Array and slices

--- Readme.md
+++ Readme.md
@@ -1,13 +1,1 @@
-#Learning Walkthrough
-
-**Hello, World!**
-- I learned
-  - instead of repeating variable in const itself, it can group it
-  - functions starts with the small letter is a private function, and starts with the big letter is a public function
-  - switch case syntax instead of nested if/else statements
-  - writing test in TDD form
-
-**Iteration**
-- I learned
-  - <code>for</code> loop
-  - how to write benchmarks
+GoLang Learning Walkthrough 
--- main.go
+++ main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"fmt"
 
-	"github.com/ayiezac78/golang-walkthrough/helloworld"
-	"github.com/ayiezac78/golang-walkthrough/integers"
-	"github.com/ayiezac78/golang-walkthrough/iteration"
+	// "github.com/ayiezac78/golang-walkthrough/helloworld"
+	// "github.com/ayiezac78/golang-walkthrough/integers"
+	// "github.com/ayiezac78/golang-walkthrough/iteration"
+	"github.com/ayiezac78/golang-walkthrough/sum"
 )
 
 func main() {
-	fmt.Println(helloworld.Hello("Ayie", "French"))
-	fmt.Println(integers.Add(2, 2))
-	fmt.Println(iteration.Repeat("a"))
+	// fmt.Println(helloworld.Hello("Ayie", "French"))
+	// fmt.Println(integers.Add(2, 2))
+	// fmt.Println(iteration.Repeat("a"))
+	numbers := []int{1, 2, 3}
+	fmt.Println(sum.SumAllTails(numbers, []int{4, 5, 6}))
 }
