a049381a8e677239e995d12181b89404daec9a81
Ariel Maniago <admaniago01@gmail.com>
2025-10-24 12:21:16 +0800

    learned iteration & benchmark

--- Readme.md
+++ Readme.md
@@ -6,3 +6,8 @@
   - functions starts with the small letter is a private function, and starts with the big letter is a public function
   - switch case syntax instead of nested if/else statements
   - writing test in TDD form
+
+**Iteration**
+- I learned
+  - <code>for</code> loop
+  - how to write benchmarks
--- main.go
+++ main.go
@@ -5,9 +5,11 @@ import (
 
 	"github.com/ayiezac78/golang-walkthrough/helloworld"
 	"github.com/ayiezac78/golang-walkthrough/integers"
+	"github.com/ayiezac78/golang-walkthrough/iteration"
 )
 
 func main() {
 	fmt.Println(helloworld.Hello("Ayie", "French"))
 	fmt.Println(integers.Add(2, 2))
+	fmt.Println(iteration.Repeat("a"))
 }
